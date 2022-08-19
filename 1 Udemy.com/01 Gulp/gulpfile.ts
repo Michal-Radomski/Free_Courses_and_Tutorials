5a32e189f29b1494497316c1225fce5e7d2afbfc
Michal Radomski <michalra@op.pl>
2022-08-19 16:26:35 +0200

    Gulp - sass task done

--- 1 Udemy.com/01 Gulp/gulpfile.ts
+++ 1 Udemy.com/01 Gulp/gulpfile.ts
@@ -1,12 +1,22 @@
 const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
 
-gulp.task("printName", async function () {
-  console.log("My name is Michal");
-});
+// gulp.task("printName", async function () {
+//   console.log("My name is Michal");
+// });
 
-gulp.task("printCountry", async function () {
-  console.log("I'm from Poland");
-});
+// gulp.task("printCountry", async function () {
+//   console.log("I'm from Poland");
+// });
 
 // gulp.task("default", gulp.parallel(["printName", "printCountry"]));
-gulp.task("default", gulp.series(["printName", "printCountry"]));
+// gulp.task("default", gulp.series(["printName", "printCountry"]));
+
+gulp.task("sass", async function () {
+  return gulp.src("./src/sass/**/*.scss").pipe(sass().on("error", sass.logError)).pipe(gulp.dest("./dist/css"));
+});
+
+// exports.buildStyles = buildStyles;
+// exports.watch = function () {
+//   gulp.watch("./sass/**/*.scss", ["sass"]);
+// };
