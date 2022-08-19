794f922ef422a467782061049bccdec095269e00
Michal Radomski <michalra@op.pl>
2022-08-19 13:45:16 +0200

    Gulp - task2 done

--- 1 Udemy.com/01 Gulp/gulpfile.ts
+++ 1 Udemy.com/01 Gulp/gulpfile.ts
@@ -3,3 +3,10 @@ const gulp = require("gulp");
 gulp.task("printName", async function () {
   console.log("My name is Michal");
 });
+
+gulp.task("printCountry", async function () {
+  console.log("I'm from Poland");
+});
+
+// gulp.task("default", gulp.parallel(["printName", "printCountry"]));
+gulp.task("default", gulp.series(["printName", "printCountry"]));
