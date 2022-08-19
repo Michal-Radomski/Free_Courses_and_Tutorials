79538b4f4316c11184a22bbc1878b72586e6ac39
Michal Radomski <michalra@op.pl>
2022-08-19 17:20:07 +0200

    gulp - watch done

--- 1 Udemy.com/01 Gulp/gulpfile.ts
+++ 1 Udemy.com/01 Gulp/gulpfile.ts
@@ -14,7 +14,22 @@ const sourcemaps = require("gulp-sourcemaps");
 // gulp.task("default", gulp.parallel(["printName", "printCountry"]));
 // gulp.task("default", gulp.series(["printName", "printCountry"]));
 
-gulp.task("sass", async function () {
+// gulp.task("sass", async function () {
+//   return gulp
+//     .src("./src/sass/**/*.scss")
+//     .pipe(sass().on("error", sass.logError))
+//     .pipe(sourcemaps.init())
+//     .pipe(
+//       autoprefixer({
+//         cascade: false,
+//         // browsers: ["last 2 versions"],
+//       })
+//     )
+//     .pipe(sourcemaps.write("."))
+//     .pipe(gulp.dest("./dist/css"));
+// });
+
+function scssTask() {
   return gulp
     .src("./src/sass/**/*.scss")
     .pipe(sass().on("error", sass.logError))
@@ -27,4 +42,15 @@ gulp.task("sass", async function () {
     )
     .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("./dist/css"));
-});
+}
+
+// File paths
+const files = {
+  scssPath: "src/sass/**/*.scss",
+};
+
+function watchTask() {
+  gulp.watch([files.scssPath], gulp.parallel(scssTask));
+}
+
+exports.default = gulp.series(gulp.parallel(scssTask), watchTask);
