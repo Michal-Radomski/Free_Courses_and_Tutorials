c0f00c82e40c17ff2e2fc49ebe72e9f505594976
Michal Radomski <michalra@op.pl>
2022-08-31 19:21:48 +0200

    DB model done

--- 1 Udemy.com/02 Node + Docker/server.ts
+++ 1 Udemy.com/02 Node + Docker/server.ts
@@ -1,12 +1,32 @@
 //- Curl Commands:
 //* curl -d '{"name": "Michal"}' -H "Content-Type: application/json" http://localhost:5000/data
+//* curl -d '{"name": "Bedroom", "serial": "001", "temperature": 26.05}' -H "Content-Type: application/json" http://localhost:5000/data
+//* curl -d '{"name": "Kitchen", "serial": "001", "temperature": 25.05}' -H "Content-Type: application/json" http://localhost:5000/data
+//* curl -d '{"name": "Salon", "serial": "001", "temperature": 22.65}' -H "Content-Type: application/json" http://localhost:5000/data
 
 const express = require("express");
 import {Request, Response} from "express";
-const {Sequelize} = require("sequelize");
+
+const {Sequelize, DataTypes} = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
   dialect: "postgres",
+});
+
+const SensorData = sequelize.define("sensor-data", {
+  // Model attributes are defined here
+  serial: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  temperature: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
 });
 
 const app = express();
@@ -19,20 +39,21 @@ const port = (process.env.PORT || 5000) as number;
 //   res.send("<h1 style='color:blue;text-align:center'>Hello, API is running</h1>");
 // });
 
-const dataList = [] as Array<any>;
-
-app.get("/data", (req: Request, res: Response) => {
+app.get("/data", async (req: Request, res: Response) => {
+  const allData = await SensorData.findAll();
   console.log("req.ip:", req.ip);
-  res.send(dataList);
+  console.log({allData});
+  res.status(200).send(allData);
   return;
 });
 
-app.post("/data", (req: Request, res: Response) => {
+app.post("/data", async (req: Request, res: Response) => {
   console.log("req.ip:", req.ip);
   let data = req.body;
   console.log({data});
-  dataList.push(data);
-  res.status(201).send(data);
+  const sensorData = await SensorData.create(data);
+  console.log({sensorData});
+  res.status(201).send(sensorData);
   return;
 });
 
@@ -43,7 +64,9 @@ app.listen({port: port}, async () => {
 
   try {
     await sequelize.authenticate();
-    console.log("Connection has been established successfully");
+    await console.log("Connection to the DB has been established successfully");
+    await sequelize.sync({alter: true});
+    await console.log("Sync to the DB");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
