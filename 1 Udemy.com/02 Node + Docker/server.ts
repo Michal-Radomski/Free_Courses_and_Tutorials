643d5f2a8a3fdee7406befbe33b602f0c98d7919
Michal Radomski <michalra@op.pl>
2022-08-31 18:54:20 +0200

    Docker - initial commit

--- 1 Udemy.com/02 Node + Docker/server.ts
+++ 1 Udemy.com/02 Node + Docker/server.ts
@@ -3,6 +3,11 @@
 
 const express = require("express");
 import {Request, Response} from "express";
+const {Sequelize} = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+  dialect: "postgres",
+});
 
 const app = express();
 app.use(express.json());
@@ -31,8 +36,15 @@ app.post("/data", (req: Request, res: Response) => {
   return;
 });
 
-app.listen({port: port}, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
+app.listen({port: port}, async () => {
+  await console.log(`Server is listening at http://localhost:${port}`);
   // For testing only
-  console.log("Current Time:", new Date().toLocaleTimeString());
+  await console.log("Current Time:", new Date().toLocaleTimeString());
+
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 });
