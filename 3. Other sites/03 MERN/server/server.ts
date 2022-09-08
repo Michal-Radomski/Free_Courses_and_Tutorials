f8a6444c01c7f129784f6b376157ad43a6c20dcc
Michal Radomski <michalra@op.pl>
2022-09-08 13:52:51 +0200

    Server - initial commit

---
+++ 3. Other sites/03 MERN/server/server.ts
@@ -0,0 +1,42 @@
+const express = require("express");
+import {Request, Response} from "express";
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+// Import routes
+
+// App
+const app = express();
+
+// Mongo DB
+// mongoose
+//   .connect(process.env.DATABASE, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then((con: {connection: {host: string}}) => {
+//     console.log(`MongoDB Database connected with HOST: ${con.connection.host}`);
+//   })
+//   .catch((error: string) => console.log("Mongo DB Error => ", error));
+
+// Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+// Test route
+app.get("/test", (req: Request, res: Response) => {
+  console.log("req.ip:", req.ip);
+  res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
+});
+//Route middleware
+
+// Port
+const port = (process.env.PORT || 5000) as number;
+
+app.listen({port: port}, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+  // For testing only
+  console.log("Current Time:", new Date().toLocaleTimeString());
+});
