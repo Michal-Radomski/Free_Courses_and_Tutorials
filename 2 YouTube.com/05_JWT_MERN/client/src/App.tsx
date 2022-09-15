178ef6dada21db81655d59afdf66d718b69f1c67
Michal Radomski <michalra@op.pl>
2022-09-15 15:22:41 +0200

    Client, Server - initial commit

--- 2 YouTube.com/05_JWT_MERN/client/src/App.tsx
+++ 2 YouTube.com/05_JWT_MERN/client/src/App.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Secret from "./pages/Secret";
 
 import "./App.scss";
 
 function App(): JSX.Element {
-  return <React.Fragment>MERN App</React.Fragment>;
+  return (
+    <React.Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Secret />} />
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ 2 YouTube.com/05_JWT_MERN/client/src/pages/Login.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = (): JSX.Element => {
+  return <React.Fragment>Login</React.Fragment>;
+};
+
+export default Login;
