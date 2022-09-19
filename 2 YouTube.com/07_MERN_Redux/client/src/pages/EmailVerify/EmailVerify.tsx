a825ad6ca23dd3a2f6edeed67c16a0842f5346de
Michal Radomski <michalra@op.pl>
2022-09-19 18:16:46 +0200

    SendEmail, EmailVerify done

--- 2 YouTube.com/07_MERN_Redux/client/src/pages/EmailVerify/EmailVerify.tsx
+++ 2 YouTube.com/07_MERN_Redux/client/src/pages/EmailVerify/EmailVerify.tsx
@@ -1,7 +1,53 @@
 import React from "react";
+import { Grid, Heading, Text } from "@chakra-ui/layout";
+import { useToast } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import { Spinner } from "@chakra-ui/spinner";
+
+import { useVerifyUserMutation } from "../../redux/api/authApi";
+import { CustomError } from "../../Interfaces";
 
 const EmailVerify = (): JSX.Element => {
-  return <React.Fragment>EmailVerify</React.Fragment>;
+  const { token } = useParams();
+  const toast = useToast();
+  console.info({ token });
+
+  const [verifyUser, { data, isError, isLoading, error, isSuccess }] = useVerifyUserMutation();
+  if (isError) {
+    toast({
+      title: (error as CustomError).data?.message,
+      status: "error",
+      duration: 5000,
+    });
+  }
+  console.info({ data });
+
+  React.useEffect(() => {
+    if (token) {
+      verifyUser({ token });
+    }
+  }, [verifyUser, token]);
+
+  return (
+    <>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          {isError && (
+            <Grid placeItems="center" h="100vh">
+              <Text>Email Verification Failed! Try Again</Text>{" "}
+            </Grid>
+          )}
+          {isSuccess && (
+            <Grid placeItems="center" h="100vh">
+              <Heading>User Verified</Heading>
+            </Grid>
+          )}
+        </>
+      )}
+    </>
+  );
 };
 
 export default EmailVerify;
--- 2 YouTube.com/07_MERN_Redux/client/src/pages/SendEmail/SendEmail.tsx
+++ 2 YouTube.com/07_MERN_Redux/client/src/pages/SendEmail/SendEmail.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { Grid, Heading, useToast } from "@chakra-ui/react";
+import { useLocation } from "react-router-dom";
+import { Spinner } from "@chakra-ui/spinner";
+
+import { useSendMailForVerificationMutation } from "../../redux/api/authApi";
+import { CustomError } from "../../Interfaces";
 
 const SendEmail = (): JSX.Element => {
-  return <React.Fragment>SendEmail</React.Fragment>;
+  const toast = useToast();
+  const { state } = useLocation();
+  console.info({ state });
+
+  const [sendMail, { isLoading, data, isError, error }] = useSendMailForVerificationMutation();
+  if (isError) {
+    toast({
+      title: (error as CustomError).data?.message,
+      status: "error",
+      duration: 5000,
+    });
+  }
+  console.info({ data });
+
+  React.useEffect(() => {
+    sendMail({ email: state.email });
+  }, [sendMail, state]);
+
+  return (
+    <>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          {" "}
+          <Grid placeItems="center" h="100vh">
+            <Heading>Email Sended</Heading>
+          </Grid>
+        </>
+      )}
+    </>
+  );
 };
 
 export default SendEmail;
