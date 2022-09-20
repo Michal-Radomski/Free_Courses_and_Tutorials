e633c322d517d68ca9e80f258e812bb589e5551b
Michal Radomski <michalra@op.pl>
2022-09-20 22:12:42 +0200

    Controllers updated

--- 2 YouTube.com/08_MERN_Redux/server/controllers/posts.ts
+++ 2 YouTube.com/08_MERN_Redux/server/controllers/posts.ts
@@ -1,11 +1,11 @@
 import { Request, RequestHandler, Response } from "express";
 import mongoose from "mongoose";
 
-// import PostMessage from "../models/PostMessage"; //* V1-> Mongoose Modele
+// import PostMessage, { IPost } from "../models/PostMessage"; //* V1-> Mongoose Modele
 import { PostMessage, IPost } from "../models/PostMessage"; //* v2 -> Typegoose Model
 import { CustomError } from "../Types";
 
-export const getPosts: RequestHandler = async (_req: Request, res: Response) => {
+export const getPosts = async (_req: Request, res: Response): Promise<void> => {
   try {
     const postMessages: IPost[] = await PostMessage.find();
 
@@ -15,7 +15,7 @@ export const getPosts: RequestHandler = async (_req: Request, res: Response) => 
   }
 };
 
-export const getPost: RequestHandler = async (req: Request, res: Response) => {
+export const getPost: RequestHandler = async (req: Request, res: Response): Promise<void> => {
   const { id } = req.params;
 
   try {
@@ -27,7 +27,7 @@ export const getPost: RequestHandler = async (req: Request, res: Response) => {
   }
 };
 
-export const createPost: RequestHandler = async (req: Request, res: Response) => {
+export const createPost: RequestHandler = async (req: Request, res: Response): Promise<void> => {
   const { title, message, selectedFile, creator, tags } = req.body;
 
   const newPostMessage: IPost = new PostMessage({ title, message, selectedFile, creator, tags });
@@ -41,11 +41,13 @@ export const createPost: RequestHandler = async (req: Request, res: Response) =>
   }
 };
 
-export const updatePost: RequestHandler = async (req: Request, res: Response) => {
+export const updatePost: RequestHandler = async (req: Request, res: Response): Promise<any> => {
   const { id } = req.params;
   const { title, message, creator, selectedFile, tags } = req.body;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with this id: ${id}`);
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send(`No post with this id: ${id}`);
+  }
 
   const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
 
@@ -53,20 +55,24 @@ export const updatePost: RequestHandler = async (req: Request, res: Response) =>
   res.json(updatedPost);
 };
 
-export const deletePost: RequestHandler = async (req: Request, res: Response) => {
+export const deletePost: RequestHandler = async (req: Request, res: Response): Promise<any> => {
   const { id } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send(`No post with id: ${id}`);
+  }
 
   await PostMessage.findByIdAndRemove(id);
 
   res.json({ message: "Post deleted successfully." });
 };
 
-export const likePost: RequestHandler = async (req: Request, res: Response) => {
+export const likePost: RequestHandler = async (req: Request, res: Response): Promise<any> => {
   const { id } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send(`No post with id: ${id}`);
+  }
 
   const post: IPost | null = await PostMessage.findById(id);
 
