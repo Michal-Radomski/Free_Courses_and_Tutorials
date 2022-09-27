209e2fb563bcb9878f86b52963b5efc2a5375686
Michal Radomski <michalra@op.pl>
2022-09-27 23:10:43 +0200

    general updates

--- 2 YouTube.com/10_Node_Passport/app.ts
+++ 2 YouTube.com/10_Node_Passport/app.ts
@@ -17,6 +17,10 @@ import userRouter from "./routes/users";
 // The server
 const app: Express = express();
 
+// EJS
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+
 // Middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -36,10 +40,6 @@ app.use(
   })
 );
 
-// EJS
-app.use(expressLayouts);
-app.set("view engine", "ejs");
-
 // Mongo DB
 mongoose
   .connect(process.env.MONGO_URL as string, {
@@ -52,11 +52,11 @@ mongoose
   })
   .catch((error: string) => console.log("Mongo DB Error => ", error));
 
-// // Test route
-// app.get("/", (req: Request, res: Response) => {
-//   console.log("req.ip:", req.ip);
-//   res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
-// });
+// Test route
+app.get("/test", (req: Request, res: Response) => {
+  console.log("req.ip:", req.ip);
+  res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
+});
 
 // Port
 const port = (process.env.PORT || 5000) as number;
