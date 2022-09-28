45a391f4c11361d089e5a9c509d0d275519df5ef
Michal Radomski <michalra@op.pl>
2022-09-28 16:34:19 +0200

    App - initial commit

--- 2 YouTube.com/11_React_i18next/src/App.tsx
+++ 2 YouTube.com/11_React_i18next/src/App.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.scss";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+
+const NotFound = (): JSX.Element => <h1 style={{ textAlign: "center", marginTop: "80px" }}>Page Not Found</h1>;
 
 function App(): JSX.Element {
-  return <React.Fragment>React App with i18next</React.Fragment>;
+  return (
+    <React.Suspense fallback={<h1 style={{ textAlign: "center", marginTop: "80px" }}>Loading...</h1>}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </React.Suspense>
+  );
 }
 
 export default App;
---
+++ 2 YouTube.com/11_React_i18next/src/pages/Profile.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profile = (): JSX.Element => {
+  return (
+    <React.Fragment>
+      <div>Profile</div>
+    </React.Fragment>
+  );
+};
+
+export default Profile;
