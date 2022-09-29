adb8d8810c632baad4eb5b753e34fe83e5803656
Michal Radomski <michalra@op.pl>
2022-09-29 23:19:15 +0200

    Client done - tutorial done

--- 2 YouTube.com/13_MERN_Nodemailer/client/src/App.tsx
+++ 2 YouTube.com/13_MERN_Nodemailer/client/src/App.tsx
@@ -1,9 +1,39 @@
 import React from "react";
+import axios from "axios";
 
 import "./App.scss";
 
 function App(): JSX.Element {
-  return <React.Fragment>App</React.Fragment>;
+  const [email, setEmail] = React.useState<string>("");
+
+  const sendEmail = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const data = {
+      email: email,
+    };
+
+    const response = await axios.post("http://localhost:5000/api/send-email", data);
+    console.log("response.data:", response.data);
+  };
+
+  return (
+    <div className="--flex-center --bg-primary --100vh">
+      <div className="--width-500px --card --p --bg-light">
+        <form className="--form-control" onSubmit={sendEmail}>
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <button type="submit" className="--btn --btn-primary">
+            Send Email
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
