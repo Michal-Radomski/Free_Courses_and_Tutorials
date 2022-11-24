6f5c6f11f3cf2ad11de4e2b4181bef0d77f510ad
Michal Radomski <michalra@op.pl>
2022-11-24 17:06:05 +0100

    schema.sql was added

--- 1 Udemy.com/05 PERN/client/src/App.tsx
+++ 1 Udemy.com/05 PERN/client/src/App.tsx
@@ -4,19 +4,40 @@ import axios from "axios";
 import "./App.scss";
 
 function App(): JSX.Element {
+  const axiosInstance = axios.create({
+    baseURL: "/api",
+    timeout: 1000,
+    headers: { "Content-Type": "application/json" },
+  });
+
   const [hello, setHello] = React.useState<string>("");
 
-  //* componentDidMount
+  //* componentDidMount in functional component
   React.useEffect(() => {
-    axios
-      .get("/api/hello")
-      .then((response) => {
-        console.log(response.data);
-        setHello(response.data.msg);
-      })
-      .catch((error) => {
-        console.log({ error });
-      });
+    // axios
+    //   .get("/api/hello")
+    //   .then((response) => {
+    //     console.log(response.data);
+    //     setHello(response.data.msg);
+    //   })
+    //   .catch((error) => {
+    //     console.log({ error });
+    //   });
+
+    const fetchData = async () => {
+      try {
+        await axiosInstance({
+          url: "/hello",
+          method: "GET",
+        }).then((response) => {
+          console.log(response.data);
+          setHello(response.data.msg);
+        });
+      } catch (error) {
+        console.error({ error });
+      }
+    };
+    fetchData();
   });
 
   return (
