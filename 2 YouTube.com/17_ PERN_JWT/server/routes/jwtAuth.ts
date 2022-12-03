233b1ffce4b84d80fbc96c878537ff30875e7ae0
Michal Radomski <michalra@op.pl>
2022-12-03 01:14:29 +0100

    Routes - initial commit

--- 2 YouTube.com/17_ PERN_JWT/server/routes/jwtAuth.ts
+++ 2 YouTube.com/17_ PERN_JWT/server/routes/jwtAuth.ts
@@ -1,5 +1,41 @@
-import express from "express";
+import express, { Request, Response, Router } from "express";
+import bcrypt from "bcrypt";
 
-const jwtAuthRouter: express.Router = express.Router();
+import pool from "../psql";
+import jwtGenerator from "../utils/jwtGenerator";
+
+const jwtAuthRouter: Router = express.Router();
+
+// Register
+jwtAuthRouter.post("/register", async (req: Request, res: Response) => {
+  const { email, name, password } = req.body;
+
+  try {
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [email]);
+    // console.log({ user });
+
+    if (user.rows.length > 0) {
+      return res.status(401).json("User already exist!");
+    }
+
+    const salt = await bcrypt.genSalt(12);
+    const bcryptPassword = await bcrypt.hash(password, salt);
+    // console.log({ salt, bcryptPassword });
+
+    let newUser = await pool.query(
+      "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, bcryptPassword]
+    );
+    // console.log({ newUser });
+
+    const jwtToken = jwtGenerator(newUser.rows[0].user_id);
+    // console.log({ jwtToken });
+
+    return res.status(200).json({ jwtToken });
+  } catch (error) {
+    console.error({ error });
+    res.status(500).send("Server error" + error);
+  }
+});
 
 export default jwtAuthRouter;
---
+++ 2 YouTube.com/17_ PERN_JWT/server/utils/jwtGenerator.ts
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const jwtGenerator = (user_id: number) => {
+  const payload = {
+    user: {
+      id: user_id,
+    },
+  };
+
+  return jwt.sign(payload, process.env.jwtSecret as string, { expiresIn: "1h" });
+};
+
+export default jwtGenerator;
