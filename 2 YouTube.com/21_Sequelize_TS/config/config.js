58e1a1feca4b3e5f76e12952059ae399962734f6
Michal Radomski <michalra@op.pl>
2022-12-15 00:58:53 +0100

    Sequelize with TS tutorial - commit2

---
+++ 2 YouTube.com/21_Sequelize_TS/config/config.js
@@ -0,0 +1,28 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const config = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+};
+
+export default config;
