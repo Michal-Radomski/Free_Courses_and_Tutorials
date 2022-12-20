8e19df4e0069f22425da6b71683c079bcf4b2a83
Michal Radomski <michalra@op.pl>
2022-12-21 00:57:58 +0100

    server.ts - initial commit

--- 2 YouTube.com/24_Auth_PSQL/indexRouter.ts
+++ 2 YouTube.com/24_Auth_PSQL/indexRouter.ts
@@ -2,4 +2,9 @@ import express, { Router, Request, Response } from "express";
 
 const indexRouter: Router = express.Router();
 
+indexRouter.get("/", async (req: Request, res: Response) => {
+  await console.log("req.ip:", req.ip);
+  await res.render("index", {});
+});
+
 export default indexRouter;
