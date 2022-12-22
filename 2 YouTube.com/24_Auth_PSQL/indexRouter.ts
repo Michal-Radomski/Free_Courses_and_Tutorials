9ea0e9550cb6f6bdd998b0e1336b78260efddf96
Michal Radomski <michalra@op.pl>
2022-12-22 01:16:09 +0100

    DB config done

--- 2 YouTube.com/24_Auth_PSQL/indexRouter.ts
+++ 2 YouTube.com/24_Auth_PSQL/indexRouter.ts
@@ -1,23 +1,28 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express, { Router, Request, Response } from "express";
+import bcrypt from "bcrypt";
+
+import pool from "./dbConfig";
 
 const indexRouter: Router = express.Router();
 
-indexRouter.get("/", async (req: Request, res: Response) => {
+indexRouter.get("/", async (req: Request, res: Response): Promise<void> => {
   await console.log("req.ip:", req.ip);
   await res.render("index", {});
 });
 
-indexRouter.get("/users/register", async (req: Request, res: Response) => {
+indexRouter.get("/users/register", async (req: Request, res: Response): Promise<void> => {
   await console.log("req.ip:", req.ip);
   await res.render("register", {});
 });
 
-indexRouter.get("/users/login", async (req: Request, res: Response) => {
+indexRouter.get("/users/login", async (req: Request, res: Response): Promise<void> => {
   await console.log("req.ip:", req.ip);
   await res.render("login", {});
 });
 
-indexRouter.get("/users/dashboard", async (req: Request, res: Response) => {
+indexRouter.get("/users/dashboard", async (req: Request, res: Response): Promise<void> => {
   await console.log("req.ip:", req.ip);
   await res.render("dashboard", { user: "Michal" });
 });
