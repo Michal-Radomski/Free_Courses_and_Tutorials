c74b973e10b25eb1bf45b509f12f477447898461
Michal Radomski <michalra@op.pl>
2022-12-30 01:44:47 +0100

    AddUser done

--- 2 YouTube.com/25_Node_MySQL/indexRouter.ts
+++ 2 YouTube.com/25_Node_MySQL/indexRouter.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
-import { findUser, viewUsers } from "./userController";
+import { addUser, findUser, formPage, viewUsers } from "./userController";
 
 const indexRouter: express.Router = express.Router();
 
 indexRouter.get("/", viewUsers);
-
 indexRouter.post("/", findUser);
+
+indexRouter.get("/adduser", formPage);
+indexRouter.post("/adduser", addUser);
 
 export default indexRouter;
--- 2 YouTube.com/25_Node_MySQL/userController.ts
+++ 2 YouTube.com/25_Node_MySQL/userController.ts
@@ -67,3 +67,37 @@ export const findUser: RequestHandler = async (req: Request, res: Response): Pro
     );
   });
 };
+
+// Add User - GET
+export const formPage: RequestHandler = async (req: Request, res: Response): Promise<void> => {
+  await console.log("req.ip:", req.ip);
+  res.render("add-user");
+};
+
+// Add new User
+export const addUser: RequestHandler = async (req: Request, res: Response): Promise<void> => {
+  await console.log("req.ip:", req.ip);
+
+  const { first_name, last_name, email, phone, comments } = req.body;
+
+  await pool.getConnection((error, connection) => {
+    if (error) {
+      console.log({ error });
+      throw error;
+    }
+    console.log(`Connected as ID: ${connection.threadId}`);
+
+    connection.query(
+      "INSERT INTO user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ?",
+      [first_name, last_name, email, phone, comments],
+      (err, rows) => {
+        if (!err) {
+          res.render("add-user", { alert: "User added successfully." });
+        } else {
+          console.log({ err });
+        }
+        console.log("The data from user table: \n", rows);
+      }
+    );
+  });
+};
