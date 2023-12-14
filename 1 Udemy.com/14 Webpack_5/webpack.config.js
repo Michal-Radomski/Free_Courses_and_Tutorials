a97832d72f86218194b2f639626f7812151642d2
Michal Radomski <michalra@op.pl>
2023-12-14 02:06:04 +0100

    Course - commit 01

--- 1 Udemy.com/14 Webpack_5/webpack.config.js
+++ 1 Udemy.com/14 Webpack_5/webpack.config.js
@@ -1,8 +1,18 @@
+//*  https://github.com/robertguss/webpack-5-fundamentals-course
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: "./src/index.js",
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
