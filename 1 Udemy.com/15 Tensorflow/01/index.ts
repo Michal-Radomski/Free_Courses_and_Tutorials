0ba38e33a4232fdda4b92bdd2593d6f2f27795b1
Michal Radomski <michalra@op.pl>
2024-04-19 00:08:09 +0200

    Tensroflow.js crash course - commit 04

--- 1 Udemy.com/15 Tensorflow/01/index.ts
+++ 1 Udemy.com/15 Tensorflow/01/index.ts
@@ -4,9 +4,18 @@ import * as tf from "@tensorflow/tfjs-node";
 // console.log("tf.getBackend():", tf.getBackend());
 // console.log("tf.memory():", tf.memory());
 
-const rank = [2, 3, 4, 5];
-const shape = [2, 2];
-const dataType = "int32";
-const tensorData = tf.tensor(rank, shape, dataType);
-tensorData.print();
-console.log("tensorData.toString()):", tensorData.toString());
+tf.tidy(() => {
+  const rank = [2, 3, 4, 5];
+  const shape = [2, 2];
+  const dataType = "int32";
+  const tensorData: tf.Tensor<tf.Rank> = tf.tensor(rank, shape, dataType);
+  tensorData.print();
+  console.log("tensorData.toString()):", tensorData.toString());
+
+  const a = tf.tensor1d([1, 2, 3, 4]);
+  const b = tf.tensor1d([10, 20, 30, 40]);
+  a.add(b).print();
+  a.sub(b).print();
+});
+
+console.log("tf.memory():", tf.memory());
