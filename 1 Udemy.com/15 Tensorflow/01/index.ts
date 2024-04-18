a9f31b0d78b97716d1bed4aa81f27f8775e77de9
Michal Radomski <michalra@op.pl>
2024-04-19 00:30:44 +0200

    Tensroflow.js crash course - commit 06

--- 1 Udemy.com/15 Tensorflow/01/index.ts
+++ 1 Udemy.com/15 Tensorflow/01/index.ts
@@ -4,21 +4,21 @@ import * as tf from "@tensorflow/tfjs-node";
 // console.log("tf.getBackend():", tf.getBackend());
 // console.log("tf.memory():", tf.memory());
 
-tf.tidy(() => {
-  const rank = [2, 3, 4, 5];
-  const shape = [2, 2];
-  const dataType = "int32";
-  const tensorData: tf.Tensor<tf.Rank> = tf.tensor(rank, shape, dataType);
-  tensorData.print();
-  console.log("tensorData.toString()):", tensorData.toString());
+// tf.tidy(() => {
+//   const rank = [2, 3, 4, 5];
+//   const shape = [2, 2];
+//   const dataType = "int32";
+//   const tensorData: tf.Tensor<tf.Rank> = tf.tensor(rank, shape, dataType);
+//   tensorData.print();
+//   console.log("tensorData.toString()):", tensorData.toString());
 
-  const a = tf.tensor1d([1, 2, 3, 4]);
-  const b = tf.tensor1d([10, 20, 30, 40]);
-  a.add(b).print();
-  a.sub(b).print();
-});
+//   const a = tf.tensor1d([1, 2, 3, 4]);
+//   const b = tf.tensor1d([10, 20, 30, 40]);
+//   a.add(b).print();
+//   a.sub(b).print();
+// });
 
-console.log("tf.memory():", tf.memory());
+// console.log("tf.memory():", tf.memory());
 
 // // Creating the tensor
 // const tr = tf.tensor([1, 2, 3, 4, 5, 6, 7]);
@@ -26,3 +26,11 @@ console.log("tf.memory():", tf.memory());
 // tr.dispose();
 // // Trying to print it now
 // tr.print();
+
+{
+  const model = tf.sequential();
+  // Add dense layer using model.add()
+  model.add(tf.layers.dense({ units: 8, inputShape: [16] }));
+  model.add(tf.layers.dense({ units: 4 }));
+  (model.predict(tf.ones([8, 16])) as tf.Tensor<tf.Rank>).print();
+}
