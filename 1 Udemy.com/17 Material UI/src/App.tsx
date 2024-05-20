ece411623ae0b3f9a278d071d1bb06855440b365
Michal Radomski <michalra@op.pl>
2024-05-21 00:49:23 +0200

    Mini MUI courses - commmit 01

--- 1 Udemy.com/17 Material UI/src/App.tsx
+++ 1 Udemy.com/17 Material UI/src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 import "./App.scss";
+import Navbar from "./components/Navbar";
 
 const App = (): JSX.Element => {
-  return <React.Fragment>App</React.Fragment>;
+  return (
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+  );
 };
 
 export default App;
---
+++ 1 Udemy.com/17 Material UI/src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = (): JSX.Element => {
+  return <React.Fragment>Navbar</React.Fragment>;
+};
+
+export default Navbar;
