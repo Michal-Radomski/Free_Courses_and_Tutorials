8d2c4804ed46ebfbe236c22415bc3df5b6d96e66
Michal Radomski <michalra@op.pl>
2025-02-02 01:36:47 +0100

    test commit4

--- 1 Udemy.com/23 Blender+3D/01/index.glsl
+++ 1 Udemy.com/23 Blender+3D/01/index.glsl
@@ -14,3 +14,38 @@ out vec4 fragColor; // Output color of the fragment
 void main() {
     fragColor = vec4(0.0, 0.58, 0.86, 1.0); // Set the fragment color (light blue)
 }
+
+#version 330 core // Specify the GLSL version
+
+layout(location = 0) in vec3 position; // Input vertex position
+
+void main() {
+    gl_Position = vec4(position, 1.0); // Set the vertex position
+}
+
+
+#version 330 core // Specify the GLSL version
+
+out vec4 fragColor; // Output color of the fragment
+
+void main() {
+    fragColor = vec4(0.0, 0.58, 0.86, 1.0); // Set the fragment color (light blue)
+}
+
+
+#version 330 core // Specify the GLSL version
+
+layout(location = 0) in vec3 position; // Input vertex position
+
+void main() {
+    gl_Position = vec4(position, 1.0); // Set the vertex position
+}
+
+
+#version 330 core // Specify the GLSL version
+
+out vec4 fragColor; // Output color of the fragment
+
+void main() {
+    fragColor = vec4(0.0, 0.58, 0.86, 1.0); // Set the fragment color (light blue)
+}
