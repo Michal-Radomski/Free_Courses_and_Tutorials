abd8056849eadf4e91c4ee8adae52400569d58ea
Michal Radomski <michalra@op.pl>
2025-02-26 00:06:52 +0100

    Litte update

--- 1 Udemy.com/23 Blender+3D/01/src/main.ts
+++ 1 Udemy.com/23 Blender+3D/01/src/main.ts
@@ -25,7 +25,7 @@ function main(): void {
 
   // Create and compile the vertex shader
   //* GLSL Shader Code
-  const vertexShaderSource = `
+  const vertexShaderSource = /*glsl*/ `
       attribute vec2 coordinates;
       void main(void) {
           gl_Position = vec4(coordinates, 0.0, 1.0);
@@ -38,7 +38,7 @@ function main(): void {
 
   // Create and compile the fragment shader
   //* Fragment Shader
-  const fragmentShaderSource = `
+  const fragmentShaderSource = /*glsl*/ `
       void main(void) {
           gl_FragColor = vec4(1.0, 0.5, 0.0, 1.0); // Orange color
       }
