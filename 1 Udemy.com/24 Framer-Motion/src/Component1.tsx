37ea50eb4475eaec105a51be61cabf85730eb8ac
Michal Radomski <michalra@op.pl>
2025-03-29 15:01:21 +0100

    Framer Motion course - commit 03

--- 1 Udemy.com/24 Framer-Motion/src/Component1.tsx
+++ 1 Udemy.com/24 Framer-Motion/src/Component1.tsx
@@ -4,6 +4,18 @@ import React from "react";
 const Component1 = (): React.JSX.Element => {
   return (
     <React.Fragment>
+      <motion.h1
+        initial={{ x: -1000, opacity: 0, scale: 0.5 }} // Starting state before mounting
+        animate={{ x: 50, opacity: 1, scale: 1, rotate: 360 }} // Animation after mounting
+        transition={{
+          duration: 2, // Duration of animation in seconds
+          ease: [0.5, 0.71, 1, 1.5], // Easing function for smooth transition
+        }}
+        whileHover={{ scale: 1.2 }} // Animation on hover
+      >
+        Animated Heading with Framer Motion
+      </motion.h1>
+
       <motion.div animate={{ x: 100, rotate: 360 }} className="box" />
     </React.Fragment>
   );
