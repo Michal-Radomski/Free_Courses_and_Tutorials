a6f413dd8fb2683912536f7d967005bae18b1bd4
Michal Radomski <michalra@op.pl>
2025-03-29 23:52:40 +0100

    Framer Motion course - commit 23

--- 1 Udemy.com/24 Framer-Motion/src/DocsExamples.tsx
+++ 1 Udemy.com/24 Framer-Motion/src/DocsExamples.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 // import { motion } from "motion/react";
 
 //* Styles
@@ -88,10 +88,63 @@ const PathDrawing = (): React.JSX.Element => {
   );
 };
 
+//* Styles
+const container: React.CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+  width: 100,
+  height: 160,
+  position: "relative",
+};
+
+const box: React.CSSProperties = {
+  width: 100,
+  height: 100,
+  backgroundColor: "#0cdcf7",
+  borderRadius: "10px",
+};
+
+const button: React.CSSProperties = {
+  backgroundColor: "#0cdcf7",
+  borderRadius: "10px",
+  padding: "10px 20px",
+  color: "#0f1115",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  right: 0,
+};
+
+const ExitAnimation = (): React.JSX.Element => {
+  const [isVisible, setIsVisible] = React.useState<boolean>(true);
+
+  return (
+    <React.Fragment>
+      <div style={container}>
+        <AnimatePresence initial={false}>
+          {isVisible ? (
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0 }}
+              style={box}
+              key="box"
+            />
+          ) : null}
+        </AnimatePresence>
+        <motion.button style={button} onClick={() => setIsVisible(!isVisible)} whileTap={{ y: 1 }}>
+          {isVisible ? "Hide" : "Show"}
+        </motion.button>
+      </div>
+    </React.Fragment>
+  );
+};
+
 const DocsExamples = (): React.JSX.Element => {
   return (
     <React.Fragment>
       <PathDrawing />
+      <ExitAnimation />
     </React.Fragment>
   );
 };
