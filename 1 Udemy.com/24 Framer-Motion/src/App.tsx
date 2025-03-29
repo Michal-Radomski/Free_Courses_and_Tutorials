613496a8ad4dee4a6c78b65d01db59b913d1549a
Michal Radomski <michalra@op.pl>
2025-03-29 14:42:26 +0100

    Framer Motion course - commit 01

---
+++ 1 Udemy.com/24 Framer-Motion/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.scss";
+import "./tailwind.css";
+
+const App = (): React.JSX.Element => {
+  return (
+    <React.Fragment>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </React.Fragment>
+  );
+};
+
+export default App;
