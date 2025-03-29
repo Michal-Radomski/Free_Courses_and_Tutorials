a62a1c5e341b9764e709a73e2b97e825416df584
Michal Radomski <michalra@op.pl>
2025-03-29 22:41:11 +0100

    Framer Motion course - commit 1

--- 1 Udemy.com/24 Framer-Motion/src/ColorChanger.tsx
+++ 1 Udemy.com/24 Framer-Motion/src/ColorChanger.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion, MotionValue, useMotionValue, useTransform } from "framer-motion";
+import { motion, MotionValue, useMotionValue, useSpring, useTransform } from "framer-motion";
 
 const ColorChanger = (): React.JSX.Element => {
   const hue: MotionValue<number> = useMotionValue(0);
@@ -8,14 +8,19 @@ const ColorChanger = (): React.JSX.Element => {
     (h: number) => `hsl(${h}, 100%, 50%)`
   );
 
+  const hue2: MotionValue<number> = useSpring(0, { stiffness: 300, damping: 30 });
+  const background: MotionValue<string> = useTransform(hue as MotionValue<number>, (h: number) => `hsl(${h}, 100%, 50%)`);
+
   const changeHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
     hue.set(parseFloat(event.target.value));
+    hue2.set(parseFloat(event.target.value));
   };
 
   return (
     <React.Fragment>
       <div>
         <motion.div className="color-box" style={{ backgroundColor, width: 200, height: 200, borderRadius: 16 }} />
+        <motion.div className="color-box" style={{ background, width: 200, height: 200, borderRadius: 32 }} />
         <div className="mt-4">
           <input type="range" min={0} max={360} step={1} defaultValue={0} onChange={changeHandler} />
         </div>
